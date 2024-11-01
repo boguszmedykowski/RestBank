89289eb64c68ff9647f713155244411e5c1c1998
Bogusz Mędykowski <bogusz.medykowski@gmail.com>
2024-11-01 12:25:55 +0100

    init project

---
+++ src/test/java/com/medykowski/RestBank/RestBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medykowski.RestBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
